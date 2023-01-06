3a0828837447a9d243ca426df809f199a182e234
Rodrigo Estabel Boquer <rodrigo-boquer@IPESAUDE.INTRA.RS.GOV.BR>
2023-01-06 17:47:09 -0300

    consegui achar como aparecer!! Mas agora dá erro no componente, estudar ele melhor ou fazer outros!

--- storybook/stories/NewTask/NewTask.stories.js
+++ storybook/stories/NewTask/NewTask.stories.js
@@ -1,11 +1,10 @@
 // NewTask.stories.js|jsx
 import React from 'react';
-import { NewTask } from '.';
+import { NewTask } from '../../../src/components/NewTask';
 //SB
 import { action } from '@storybook/addon-actions';
 import { text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react-native';
-import React from 'react';
 import { Text } from 'react-native';
 import { CenterView } from "../CenterView"
 
